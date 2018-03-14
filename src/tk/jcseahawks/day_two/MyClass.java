8cef3a1f8ef1d191d2cbcb99dcaba8d3188af7db
jcseahawks <damned0072002@gmail.com>
2018-03-14 16:30:13 -0400

    added comments for using static imports

--- src/tk/jcseahawks/day_two/MyClass.java
+++ src/tk/jcseahawks/day_two/MyClass.java
@@ -1,7 +1,17 @@
 package tk.jcseahawks.day_two;
 
 public class MyClass {
+
+//could as well be written as
+
+//static public void main(String[] args){
+
+//public static void main(String... x){
+
+//static public void main(String bang_a_gong[]){ 
+   
   public static void main(String[] args){
      System.out.println(args[0] + " " + args[1]);
+     
   }
 }

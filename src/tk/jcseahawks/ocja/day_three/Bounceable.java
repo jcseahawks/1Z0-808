fdaf1b6492ae9b46db6a05535b6cc1a35ac14506
jcseahawks <damned0072002@gmail.com>
2018-03-18 15:20:41 -0400

    Late commit of streak day three

--- src/tk/jcseahawks/ocja/day_three/Bounceable.java
+++ src/tk/jcseahawks/ocja/day_three/Bounceable.java
@@ -1,8 +1,7 @@
 package tk.jcseahawks.ocja.day_three;
-
 //Sat Mar 17 08:11:49 EDT 2018
-
-interface Bounceable {
-    void bounce();
-    void setBounceFactor(int bf);
+//public abstract interface implicitly
+interface Bounceable { 
+    void bounce(); // public abstract implicitly
+    void setBounceFactor(int bf); // public abstract implicitly
 }

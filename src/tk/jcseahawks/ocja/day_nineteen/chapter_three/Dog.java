c6ea7df51a9faad4f4a7df6a3f5f57dc8e091224
jcseahawks <damned0072002@gmail.com>
2018-05-20 13:12:49 -0400

    Started Chapter Three

---
+++ src/tk/jcseahawks/ocja/day_nineteen/chapter_three/Dog.java
@@ -0,0 +1,21 @@
+package tk.jcseahawks.ocja.day_nineteen.chapter_three;
+// Sun May 20 12:52:15 EDT 2018
+class Collar { }
+class Dog {
+	Collar c;                         // instance variable
+	String name;                      // instance variable
+	
+	public static void main(String [] args) {
+		Dog d;                    // local variable: d
+		d = new Dog();
+		d.go(d);
+	}
+	void go(Dog dog) {                // local variable: dog
+		c = new Collar();
+		dog.setName("Aiko");
+	}
+	void setName(String dogName) {    // local var: dogName
+		name = dogName;
+		// do more stuff
+	}
+}

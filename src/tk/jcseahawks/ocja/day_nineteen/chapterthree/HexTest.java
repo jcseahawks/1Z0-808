2a4a2c4a1e6d61a0ee62e7e31323c14e00c38474
jcseahawks <damned0072002@gmail.com>
2018-07-09 15:30:45 -0400

    modified .gitignore

---
+++ src/tk/jcseahawks/ocja/day_nineteen/chapterthree/HexTest.java
@@ -0,0 +1,10 @@
+package tk.jcseahawks.ocja.day_nineteen.chapterthree;
+// Sun May 20 13:53:37 EDT 2018
+class HexTest {
+	public static void main (String [] args) {
+		int x = 0X0001;
+		int y = 0x7fffffff;
+		int z = 0xDeadCafe;
+		System.out.println("x = " + x + " y = " + y + " z = " + z);
+	}
+}
---
+++ src/tk/jcseahawks/ocja/day_nineteen/chapterthree/Octal.java
@@ -0,0 +1,12 @@
+package tk.jcseahawks.ocja.day_nineteen.chapterthree;
+// Sun May 20 13:37:32 EDT 2018
+class Octal {
+	public static void main(String [] args) {
+		int six = 06;    // Equal to decimal 6
+		int seven = 07;  // Equal to decimal 7
+		int eight = 010; // Equal to decimal 8
+		int nine = 011;  // Equal to decimal 9
+		System.out.println("Octal 010 = " + eight);
+	}
+}
+
